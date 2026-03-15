eaa083fe4670bd9eab987c2c5905801520c70979
Scott Shellabarger <scott@thoughtafter.com>
2026-03-15 15:37:28 -0500

    Deleted old files

--- tests/test_imports.py
+++
@@ -1,5 +0,0 @@
-def test_import():
-    import trading_bot
-    from trading_bot.engine import Trader
-    from trading_bot.webapp import app
-    assert callable(Trader)
--- tests/test_util_import.py
+++
@@ -1,6 +0,0 @@
-def test_imports():
-    import trading_bot
-    from trading_bot.engine import Trader
-    from trading_bot.webapp import app
-    assert callable(Trader)
-    assert app is not None
